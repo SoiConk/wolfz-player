718ac26f54ea49ae4640a585e2a586fe30ce5e82
unknown <thaiduybao01022005@gmail.com>
2026-07-14 21:28:42 +0700

    chore: update build config and main entry

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QIcon>
+#include "control/service/MetadataManager.h"
+#include "data/playlist/Queue.h"
+#include "data/playlist/History.h"
+#include <QQuickStyle>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    QQuickStyle::setStyle("Basic");
+
     app.setWindowIcon(QIcon(":/Blueberry_Wolfz/src/ui/assets/images/appIcon.png"));
+
+    MetadataManager& instance = MetadataManager::getInstance();
+    History::getInstance().addList(instance.getHistory());
+    Queue::getInstance().addList(instance.getQueue());
 
     QQmlApplicationEngine engine;
 
-    // Load UI
-    engine.load(QUrl(QStringLiteral("qrc:/Blueberry_Wolfz/src/ui/Main.qml")));
+    engine.loadFromModule("Blueberry_Wolfz", "Main");
 
     if (engine.rootObjects().isEmpty())
         return -1;
